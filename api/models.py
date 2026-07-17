331c88c954f95e5f0f1ab3ceb2823f892b86c721
Kevinn045 <98132402+Kevinn045@users.noreply.github.com>
2026-07-17 02:22:28 -0700

    Add created field and update ordering in Project model

--- api/models.py
+++ api/models.py
@@ -4,13 +4,15 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    link = models.URLField()
-    image = models.ImageField(upload_to='projects/', null=True, blank=True)
     technologies = models.CharField(max_length=255)
     github = models.URLField(blank=True)
     live_demo = models.URLField(blank=True)
+    image = models.ImageField(upload_to='projects/', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-       ordering = ['-id']
+        ordering = ['-created']
+
     def __str__(self):
         return self.title
 
