0975900afc2d013d00dfef4512a20f9537bdf4e5
Kevinn045 <98132402+Kevinn045@users.noreply.github.com>
2026-07-17 02:33:07 -0700

    Refactor serializers to use explicit fields

--- api/serializers.py
+++ api/serializers.py
@@ -1,32 +1,55 @@
-from rest_framework import serializers
-from .models import Contact, Project
 from django.contrib.auth.models import User
-from .models import Blog
-
+from rest_framework import serializers
+from .models import Blog, Contact, Project
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = '__all__'
-
+        fields = [
+            "id",
+            "title",
+            "content",
+            "image",
+            "created",
+            "updated",
+        ]
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all__'
-
+        fields = [
+            "id",
+            "title",
+            "description",
+            "technologies",
+            "github",
+            "live_demo",
+            "image",
+            "created",
+        ]
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = '__all__'
-
+        fields = [
+            "id",
+            "name",
+            "email",
+            "message",
+            "created",
+            "is_read",
+        ]
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 8
+                }
+        }
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
