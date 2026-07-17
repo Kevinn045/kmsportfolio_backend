8cb9df0f604728d24f5e67807bf11a2d8e1867f2
Kevinn045 <98132402+Kevinn045@users.noreply.github.com>
2026-07-17 02:14:19 -0700

    Add created and is_read fields to Contact model

--- api/models.py
+++ api/models.py
@@ -19,7 +19,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
-
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 
